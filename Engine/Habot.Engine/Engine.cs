a13780ab7e610ab524144d6975b40ca4d72f4e26
maksimowiczm <maksimowiczmateusz13@gmail.com>
2024-01-24 12:21:41 +0100

    Depth 1 evaluation

--- Engine/Habot.Engine/Engine.cs
+++ Engine/Habot.Engine/Engine.cs
@@ -1,5 +1,7 @@
+using System.Collections.Immutable;
 using Habot.Core.Board;
 using Habot.Core.Engine;
+using Habot.Core.Mailbox;
 using Habot.UCI.Notation;
 using Habot.UCI.Request;
 
@@ -9,8 +11,50 @@ public class Engine<TGenerator, TBoard> : IEngine<TGenerator, TBoard>
     where TGenerator : IMoveGenerator
     where TBoard : IBoard, IMementoBoard, IPlayableBoard
 {
+    private static double Value((Square, Piece) item)
+    {
+        var (square, piece) = item;
+
+        return piece.Type switch
+        {
+            PieceType.King => 0,
+            PieceType.Queen => 9,
+            PieceType.Rook => 5,
+            PieceType.Bishop => 3,
+            PieceType.Knight => 3,
+            PieceType.Pawn => 1,
+        };
+    }
+
+    private double Evaluate(TBoard board, Color color)
+    {
+        var pieces = board.Pieces
+            .GroupBy(g => g.Item2.Color)
+            .ToImmutableDictionary(g => g.Key, g => g.ToImmutableList());
+
+        return evaluate(color) - evaluate(color.Toggle());
+
+        double evaluate(Color color) => pieces[color].Select(Value).Sum();
+    }
+
     public Move Search(Go request, TGenerator moveGenerator, TBoard board)
     {
-        throw new NotImplementedException();
+        var legalMoves = moveGenerator.GetLegalMoves().ToList();
+
+        var evaluations = legalMoves
+            .Select(move =>
+            {
+                board.Save(move);
+                board.Move(move);
+
+                // -evaluation due to color change after move
+                var evaluation = -Evaluate(board, board.ColorToMove);
+                board.Restore();
+                return (move, evaluation);
+            });
+
+        var bestMove = evaluations.MaxBy(v => v.evaluation);
+
+        return bestMove.move;
     }
 }
